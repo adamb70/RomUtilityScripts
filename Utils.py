4dc30b162767a1e38c494ea6764007f15e17e1aa
adamb70 <adam.boaler@hotmail.com>
2018-02-18 01:03:25 +0000

    Tabs to spaces

--- Utils.py
+++ Utils.py
@@ -1,12 +1,16 @@
-def indent(elem, level=0):
+def indent(elem, level=0, tabs=False):
     """ https://stackoverflow.com/a/33956544/4796605 """
-    i = "\n" + level * "\t"
+    if tabs:
+        spacer = "\t"
+    else:
+        spacer = "    "
+    i = "\n" + level * spacer
     if len(elem):
         if not elem.text or not elem.text.strip():
-            elem.text = i + "\t"
+            elem.text = i + spacer
         if not elem.tail or not elem.tail.strip():
             if level <= 1:
-                elem.tail = i + "\n" + "\t" * level
+                elem.tail = i + "\n" + spacer * level
             else:
                 elem.tail = i
         for elem in elem:
