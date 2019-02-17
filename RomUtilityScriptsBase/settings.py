84a0f4befe95aefca0e99bb9ecaca70ba6abdaa3
adamb70 <adam.boaler@hotmail.com>
2019-02-17 03:06:18 +0000

    package test

--- RomUtilityScriptsBase/settings.py
+++
@@ -1,7 +0,0 @@
-from os import path
-
-proj_path = path.abspath(path.dirname(__file__))
-KEYFILE = path.join(proj_path, 'Mistvalin-b3c187e87518.json')
-KEYFILE_TEXT = None
-
-GITHUB_TOKEN = None
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='RomUtilityScripts',
+      version='0.1',
+      description='RomUtilityScripts',
+      url='http://github.com/adamb70/RomUtilityScripts',
+      author='adamb70',
+      author_email='',
+      license='MIT',
+      packages=['RomUtilityScripts'],
+      zip_safe=False)
