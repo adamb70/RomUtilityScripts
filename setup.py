b4cf055c71f391356fa28894ec9ffe45ec6f35b2
adamb70 <adam.boaler@hotmail.com>
2019-02-21 01:58:03 +0000

    Package test

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='RomUtilityScripts',
       version='0.1.1',
@@ -7,5 +7,5 @@ setup(name='RomUtilityScripts',
       author='adamb70',
       author_email='',
       license='MIT',
-      packages=['RomUtilityScripts'],
+      packages=find_packages(),
       zip_safe=False)
