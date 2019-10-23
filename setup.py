15bcdbb6524efd8741ef550af348b65770e46a19
adamb70 <adam.boaler@hotmail.com>
2019-10-23 14:20:38 +0100

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='RomUtilityScripts',
-      version='0.1.5',
+      version='0.2.2',
       description='RomUtilityScripts',
       url='http://github.com/adamb70/RomUtilityScripts',
       author='adamb70',
